82d4c5376d2785eda778f6aa81ddb90285185cea
Fatimezzahradarim <fatimezzahradarim@gmail.com>
2023-04-01 06:44:13 +0000

    Second  class with 2 methods

--- src/domain/Operations.java
+++ src/domain/Operations.java
@@ -1,5 +1,15 @@
 package domain;
 
-public class Operations {
+    public class Operations {
+        //Methode de l'addition
+        public static void deposerSolde(Compte compte, int montant) {
+            int soldeActuel = compte.getSoldeCompte();
+            compte.setSoldeCompte(soldeActuel + montant);
+        }
+         //Methode de la soustraction
+        public static void retirerSolde(Compte compte, int montant) {
+            int soldeActuel = compte.getSoldeCompte();
+            compte.setSoldeCompte(soldeActuel - montant);
+        }
+    }
 
-}
